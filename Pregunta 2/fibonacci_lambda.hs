f37b322e7e56dbcfe133fbb3b45378fb19ef0fd6
Augusto Rios <56129071+SphinxBSD@users.noreply.github.com>
2022-06-13 19:24:48 -0400

    Update fibonacci_lambda.hs

--- Pregunta 2/fibonacci_lambda.hs
+++ Pregunta 2/fibonacci_lambda.hs
@@ -1,10 +1,25 @@
-fib = fst . fib2
+import Prelude hiding (pred)
 
--- | Return (fib n, fib (n + 1))
-fib2 0 = (1, 1)
-fib2 1 = (1, 2)
-fib2 n
- | even n    = (a*a + b*b, c*c - a*a)
- | otherwise = (c*c - a*a, b*b + c*c)
- where (a,b) = fib2 (n `div` 2 - 1)
-       c     = a + b
+data Nat = Succ Nat | Zero deriving Show
+
+pred (Succ pred) = pred
+pred Zero        = Zero
+
+fold (Succ pred) succ zero = succ (fold pred succ zero)
+fold Zero succ zero        = zero
+
+add a b = fold a Succ b
+
+fromInt 0 = Zero
+fromInt x = Succ (fromInt (x-1))
+
+toInt x = fold x (+ 1) 0
+
+fib n = fold n fib' id (pred n) where
+    fib' rec (Succ p) = add (rec p) (rec (pred p))
+    fib' rec Zero     = Succ Zero
+
+main = print $ toInt (fib (fromInt 30))
+
+fib = (λa.(a(λbc.(c(λdefg.(bef(b(e(λhi.i)(λhij.(j(λk.(kij)))))fg)))(λdef.(ef))))
+        (λb.b)(a(λbcd.(c(λe.(ecd))b))(λbc.(c(λd.(dbc))))(λbc.c)(λbcd.(d(λe.(ecd)))))))
